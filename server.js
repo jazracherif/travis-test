4efe2cfdef7c2b931393ad085183741396f3188c
Cherif Jazra <jazracherif@gmail.com>
2020-07-27 12:21:16 -0700

    add env var check

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
-    await sleep(5000);
+    console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(1000);
   }
 }
 
 main();
+
